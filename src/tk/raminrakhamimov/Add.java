46c404317762fcfaa427ec6965945b22a5b6f2da
Ramin Rakhamimov <ramin32@gmail.com>
2009-08-16 07:55:29 -0400

    Ajax is now working.

--- src/tk/raminrakhamimov/Add.java
+++ src/tk/raminrakhamimov/Add.java
@@ -7,13 +7,26 @@ import javax.servlet.*;
 
 public class Add extends HttpServlet 
 {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) 
+    throws ServletException, IOException
+    {
+        doPost(request, response);
+    }
+
     public void doPost(HttpServletRequest request, HttpServletResponse response) 
     throws ServletException, IOException
     {
         PrintWriter out = response.getWriter();
         
-        int first = Integer.parseInt(request.getParameter("first"));
-        int second = Integer.parseInt(request.getParameter("second"));
+        int first = 0;
+        int second = 0;
+        try
+        {
+            first = Integer.parseInt(request.getParameter("first"));
+            second = Integer.parseInt(request.getParameter("second"));
+        }
+        catch(Exception e){}
+
         out.println(first + second);
         out.close();
     }
